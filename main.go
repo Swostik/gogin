c8ee0a375ec6640f49e9aa74b89a588715f52c69
Swostik <slokk03@gmail.com>
2022-02-07 11:32:56 +0545

    code formatter

--- main.go
+++ main.go
@@ -6,48 +6,46 @@ import (
 )
 
 type User struct {
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
 	PersonalCode string `json:"personalCode"`
 }
 
 var Users []User
 
-func main()  {
+func main() {
 	r := gin.Default()
-	r.GET("/users",GetAll) 
+	r.GET("/users", GetAll)
 	r.POST("/users", CreateUser)
 	r.PUT("/users/:personalCode", EditUser)
 	r.DELETE("/users/:personalCode", DeleteUSer)
 	//fmt.Println("hello")
 	r.Run()
-	
+
 }
 
-func GetAll(c *gin.Context)  {
-	
-		c.JSON(200, Users)
-	
+func GetAll(c *gin.Context) {
+
+	c.JSON(200, Users)
+
 }
 
-func CreateUser(c *gin.Context){
+func CreateUser(c *gin.Context) {
 	var reqBody User
 	if err := c.ShouldBindJSON(&reqBody); err != nil {
 		c.JSON(422, gin.H{
-			"error": true,
+			"error":   true,
 			"message": "invalid request body",
 		})
 		return
-		
+
 	}
 	reqBody.PersonalCode = uuid.New().String()
 	Users = append(Users, reqBody)
 
 	c.JSON(200, Users)
 
-	
 	//fmt.Println(reqBody)
-	
 
 }
 
@@ -56,11 +54,11 @@ func EditUser(c *gin.Context) {
 	var reqBody User
 	if err := c.ShouldBindJSON(&reqBody); err != nil {
 		c.JSON(422, gin.H{
-			"error": true,
+			"error":   true,
 			"message": "invalid request body",
 		})
 		return
-		
+
 	}
 
 	for i, u := range Users {
@@ -71,14 +69,13 @@ func EditUser(c *gin.Context) {
 			c.JSON(200, gin.H{
 				"error": false,
 			})
-		return
-	
-		
+			return
+
 		}
 	}
-	c.JSON(400, gin.H {
-		"error":true,
-		"message":"invalid personalCode",
+	c.JSON(400, gin.H{
+		"error":   true,
+		"message": "invalid personalCode",
 	})
 
 }
@@ -88,7 +85,7 @@ func DeleteUSer(c *gin.Context) {
 
 	for i, u := range Users {
 		if u.PersonalCode == id {
-			Users = append(Users[:i], Users[i+1:]...) 
+			Users = append(Users[:i], Users[i+1:]...)
 			c.JSON(200, gin.H{
 				"error": false,
 			})
@@ -97,7 +94,7 @@ func DeleteUSer(c *gin.Context) {
 	}
 
 	c.JSON(404, gin.H{
-		"error":true,
+		"error":   true,
 		"message": "invalid code",
 	})
 
